e4b6a67894203bbc46dfef311b58d4f10883fc38
chopade.ritesh23 <chopade.ritesh23@gmail.com>
2012-03-08 13:43:59 +0000

    Added easy validation showcase example

---
+++ easy-validation-showcase/WEB-INF/src/com/easyvalidation/showcase/UserDto.java
@@ -0,0 +1,95 @@
+package com.easyvalidation.showcase;
+
+public class UserDto {
+
+	private String name;
+
+	private String age;
+
+	private String email;
+
+	private String dob;
+
+	private String zipcode;
+
+	private String url;
+
+	private String creditcardNo;
+
+	private String isbn;
+
+	private String inetaddress;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getCreditcardNo() {
+		return creditcardNo;
+	}
+
+	public void setCreditcardNo(String creditcardNo) {
+		this.creditcardNo = creditcardNo;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getInetaddress() {
+		return inetaddress;
+	}
+
+	public void setInetaddress(String inetaddress) {
+		this.inetaddress = inetaddress;
+	}
+
+}
